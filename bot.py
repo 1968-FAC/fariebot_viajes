563b10990dd34863da757add4361a627011f4487
FABIO ARIEL <arqfabiocervantes@gmail.com>
2025-06-18 13:49:02 -0500

    Fix: corrección bot.py con ApplicationBuilder para ptb 20.7

--- bot.py
+++ bot.py
@@ -3,12 +3,11 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from dotenv import load_dotenv
 
-# Cargar las variables del archivo .env
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("¡Hola! Soy tu bot de viajes ✈️🌍. ¿En qué puedo ayudarte?")
+    await update.message.reply_text("¡Hola! Soy tu bot de viajes ✈🌍. ¿En qué puedo ayudarte?")
 
 if __name__ == '__main__':
     app = ApplicationBuilder().token(TOKEN).build()
